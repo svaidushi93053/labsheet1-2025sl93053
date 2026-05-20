82061b3d6c3be324f479b84618e007263233a6ad
svaidushi93053 <2025sl93053@wilp.bits-pilani.ac.in>
2026-05-20 20:56:20 +0530

    Add Dockerfile and update calculator.py for Lab Sheet 3

--- calculator.py
+++ calculator.py
@@ -13,6 +13,11 @@ def divide(a,b):
     return None
   return a/b
 
-
+if __name__ == "__main__":
+    print("=== Calculator Results ===")
+    print("Add      10 + 5 =", add(10, 5))
+    print("Subtract 10 - 5 =", subtract(10, 5))
+    print("Multiply 10 * 5 =", multiply(10, 5))
+    print("Divide   10 / 5 =", divide(10, 5))
 
 
